c64753b3f3b8a2ba959dfaddfd861c747f0bd8f2
sinthyasithy <145050483+sinthyasithy@users.noreply.github.com>
2024-04-29 21:34:57 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//22.04.24-arithmetic=
+let x = 5;
+x += 5;
+console.log('value of x is :'+x);
+
+// addition+
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+
+console.log("The sum of", num1, "and", num2, "is:", sum);
+
+//Multiplication*
+let first = 7;
+let second = 3;
+
+let product = first * second ;
+
+console.log("The product of", first, "and", second, "is:", product);
+
+// Comparison 
+let operand1 = 20;
+let operand2 = 10;
+
+let isGreaterThan = operand1 > operand2;
+console.log(operand1, "is greater than", operand2, ":", isGreaterThan);
